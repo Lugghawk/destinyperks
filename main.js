95b2ee05c1c577b8f40cf599c7a0654b0e2d3f38
Lugg, Dave <acidictadpole@gmail.com>
2016-05-27 10:43:44 -0400

    Added CSS to underline perks that were modified. Removed typo'd filename

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var replacePerks = function(element){
 
 	for (var i = 0; i < perks.length; i++){
 		var perk = perks[i];
-		element.innerHTML = element.innerHTML.replace(perk.displayName, "<span title='"+perk.displayDescription+"'>"+perk.displayName+"</span>");
+		element.innerHTML = element.innerHTML.replace(perk.displayName, '<span title="'+perk.displayDescription+'" class="destinyperk">'+perk.displayName+'</span>');
 	}
 	return element;
 };
